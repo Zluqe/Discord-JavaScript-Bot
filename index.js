bd17a2884b6123024a34ce9dc461621efe3aff21
cryart <68236130+Gaming552@users.noreply.github.com>
2023-02-03 00:02:21 +0200

    I made it load the commands, events and handlers

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// this is a file
 const {token, prefix} = require("./config.json");
 const Discord = require("discord.js");
 const random = require("lodash");
+
+const prefix = prefix;
 
 const client = new Discord.Client({
   ws: { intents: Discord.Intents.ALL },
@@ -15,6 +16,27 @@ const statuses = [
   "Created by ${created.username}",
   "Modify me"
 ];
+
+const commands = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
+const events = fs.readdirSync("./events").filter((file) => file.endsWith(".js"));
+const handlers = fs.readdirSync("./handlers").filter((file) => file.endsWith(".js"));
+
+for (const file of commands) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+for (const file of events) {
+  const event = require(`./events/${file}`);
+  const eventn = file.split(".")[0];
+  client.on(eventn, event.bind(null, client));
+}
+
+for (const file of handlerFiles) {
+  const handler = require(`./handlers/${file}`);
+  const handlern = file.split(".")[0];
+  client[handlern] = handler;
+}
 
 client.on("ready", () => {
   console.log(`${client.user.tag} is online!`);
