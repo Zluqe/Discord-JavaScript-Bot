8746ce1b21adb1a31021a27574aeaf4afaf0e9cd
cryart <68236130+Gaming552@users.noreply.github.com>
2023-02-02 23:25:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 // this is a file
 const {token, prefix} = require("./config.json");
 const Discord = require("discord.js");
+const random = require("lodash");
+
+const client = new Discord.Client({
+  ws: { intents: Discord.Intents.ALL },
+});
+
+const statuses = [
+  "Host with Zluqe",
+  "Created by 5ws",
+  "Modify me"
+];
+
+client.on("ready", () => {
+  console.log(`${client.user.tag} is online!`);
+
+  setInterval(() => {
+    const status = random.sample(statuses);
+    client.user.setPresence({ activity: { name: status }, status: "online" });
+  }, 3000);
+});
+
+client.login(token);
