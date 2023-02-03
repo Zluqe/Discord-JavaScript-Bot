98a224450943caf2a9fbd57ddc3289afac74ff78
cryart <68236130+Gaming552@users.noreply.github.com>
2023-02-03 11:58:41 +0200

    Update mention.js

--- events/mention.js
+++ events/mention.js
@@ -1,7 +1,5 @@
 const Discord = require("discord.js");
 
-const prefix = prefix;
-
 module.exports = (client) => {
   client.on("message", (message) => {
     if (message.mentions.users.has(client.user.id)) {
