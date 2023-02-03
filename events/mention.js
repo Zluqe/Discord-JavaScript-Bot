6af24193bf2b3c5e3d9479b065157988995cbfad
cryart <68236130+Gaming552@users.noreply.github.com>
2023-02-03 10:00:03 +0200

    When you mention the bot, it shows the prefix

--- events/mention.js
+++ events/mention.js
@@ -1,9 +1,12 @@
+const {prefix} = require("./config.json");
 const Discord = require("discord.js");
+
+const prefix = prefix;
 
 module.exports = (client) => {
   client.on("message", (message) => {
     if (message.mentions.users.has(client.user.id)) {
-      message.reply("My prefix is ");
+      message.reply("My prefix is ${prefix}");
     }
   });
 };
