8b1d77bd7c610dbd8a760029e8494ae9b97d4f70
cryart <68236130+Gaming552@users.noreply.github.com>
2023-02-03 09:33:15 +0200

    Ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,34 @@
+const Discord = require("discord.js");
 
+client.command = new Discord.Collection();
+
+const ping = {
+  name: "ping",
+  description: "Show bot latency",
+  execute(message) {
+    const latency = Math.round(client.ws.ping);
+    message.channel.send(`Ping: `${latency}`ms`);
+  },
+};
+
+module.exports = ping;
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const commandn = args.shift().toLowerCase();
+
+  const command =
+    client.commands.get(commandn) ||
+    client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandn));
+
+  if (!command) return;
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
+  }
+});
