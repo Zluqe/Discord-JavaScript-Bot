a13e68a575e4b50050f5b1ecb19776eef30bebc7
cryart <68236130+Gaming552@users.noreply.github.com>
2023-02-03 10:03:13 +0200

    Update mention.js

--- events/mention.js
+++ events/mention.js
@@ -5,7 +5,7 @@ const prefix = prefix;
 module.exports = (client) => {
   client.on("message", (message) => {
     if (message.mentions.users.has(client.user.id)) {
-      message.reply("My prefix is \`${client.prefix}\`");
+      message.reply(`My prefix is \`${client.prefix}\``);
     }
   });
 };
