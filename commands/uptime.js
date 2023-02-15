457362814138caf0b6518c28cd312e97c036ea57
cryart <68236130+Gaming552@users.noreply.github.com>
2023-02-15 22:26:03 +0200

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -5,11 +5,11 @@ module.exports = {
   name: "uptime",
   description: "View uptime.",
   execute(message) {
-    const uptimeSeconds = Math.floor(process.uptime());
-    const days = Math.floor(uptimeSeconds / (24 * 60 * 60));
-    const hours = Math.floor((uptimeSeconds % (24 * 60 * 60)) / (60 * 60));
-    const minutes = Math.floor((uptimeSeconds % (60 * 60)) / 60);
-    const seconds = Math.floor(uptimeSeconds % 60);
+    const uptime = Math.floor(process.uptime());
+    const days = Math.floor(uptime / (24 * 60 * 60));
+    const hours = Math.floor((uptime % (24 * 60 * 60)) / (60 * 60));
+    const minutes = Math.floor((uptime % (60 * 60)) / 60);
+    const seconds = Math.floor(uptime % 60);
 
     const embed = new MessageEmbed()
       .setDescription(`**${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds**`)
