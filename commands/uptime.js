66c9d217c75b059cdb36fea687ab6ab0c2e7b572
cryart <68236130+Gaming552@users.noreply.github.com>
2023-02-15 22:25:10 +0200

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,10 +1,20 @@
 const Discord = require("discord.js");
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   name: "uptime",
   description: "View uptime.",
-  execute(message, args) {
-    const client = message.client;
-    const data = [];
-    const { commands } = message.client;
+  execute(message) {
+    const uptimeSeconds = Math.floor(process.uptime());
+    const days = Math.floor(uptimeSeconds / (24 * 60 * 60));
+    const hours = Math.floor((uptimeSeconds % (24 * 60 * 60)) / (60 * 60));
+    const minutes = Math.floor((uptimeSeconds % (60 * 60)) / 60);
+    const seconds = Math.floor(uptimeSeconds % 60);
 
+    const embed = new MessageEmbed()
+      .setDescription(`**${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds**`)
+      .setColor('#00ff00');
+
+    message.channel.send({ embeds: [embed] });
+  },
+};
