d146f984bcd21ad2e884e63b432eb0a9494a9c0b
cryart <68236130+Gaming552@users.noreply.github.com>
2023-02-15 22:03:22 +0200

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,1 +1,10 @@
-y
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "uptime",
+  description: "View uptime.",
+  execute(message, args) {
+    const client = message.client;
+    const data = [];
+    const { commands } = message.client;
+
